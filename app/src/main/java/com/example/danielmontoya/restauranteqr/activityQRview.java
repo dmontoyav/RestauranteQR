e7466e0744efc7a911d1ffe3778b63b50b6a0060
d.montoyav <d.montoyav@uniandes.edu.co>
2020-10-07 15:34:02 -0500

    Version 1.0 funcional. Falta opción share. Poner nombre de archivo cuando entra en la foto. Poner sombra en los botones

--- app/src/main/java/com/example/danielmontoya/restauranteqr/activityQRview.java
+++ app/src/main/java/com/example/danielmontoya/restauranteqr/activityQRview.java
@@ -1,14 +1,17 @@
 package com.example.danielmontoya.restauranteqr;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
 import java.io.IOException;
 
 public class activityQRview extends AppCompatActivity implements View.OnClickListener {
@@ -23,7 +26,7 @@ public class activityQRview extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_qrview);
         String uri = getIntent().getStringExtra("fileUri");
         System.out.println(uri);
-        uri = "file://"+uri;
+        //uri = "file://"+uri;
         fileUri = Uri.parse(uri);
         System.out.println(fileUri);
         imageView = (ImageView) findViewById(R.id.imagen_QR);
@@ -50,5 +53,15 @@ public class activityQRview extends AppCompatActivity implements View.OnClickLis
 
     private void compartirQR() {
 
+        Intent shareIntent = new Intent();
+        Uri Uri = FileProvider.getUriForFile(getApplicationContext(), "com.myfileprovider", new File(fileUri.));
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+        shareIntent.setType("image/*");
+        startActivity(Intent.createChooser(shareIntent, "Compartir"));
+
+
     }
+
 }
